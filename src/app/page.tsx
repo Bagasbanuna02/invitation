16800f2ee0ca547675e038dc3a12622372ed2768
Bagasbanuna02 <bagasbanuna02@gmail.com>
2025-06-15 23:18:02 +0800

    fix folder landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { Header } from "@/components/header";
+import { Header } from "@/components/LadingPage/header";
 import { AppShell } from "@mantine/core";
-import { FeatureSection } from "@/components/feature-section";
-import { Footer } from "@/components/footer";
-import { HeroSection } from "@/components/hero-section";
-import { PricingSection } from "@/components/pricing-section";
-import { RegistrationNowSection } from "@/components/registation.now-section";
+import { FeatureSection } from "@/components/LadingPage/feature-section";
+import { Footer } from "@/components/LadingPage/footer";
+import { HeroSection } from "@/components/LadingPage/hero-section";
+import { PricingSection } from "@/components/LadingPage/pricing-section";
+import { RegistrationNowSection } from "@/components/LadingPage/registation.now-section";
 
 export default function HomePage() {
   return (
